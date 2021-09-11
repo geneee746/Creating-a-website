f74355590aeac348d4eccc8ff21a26a7c29da644
GeneEe <75520104+geneee746@users.noreply.github.com>
2021-09-11 03:24:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,87 @@
+console.log("Hello world!");
+
+console.log("My name is Osman");
+
+let myName = "Bob";
+let myAge = 17;
+let isHappy = true;
+
+let radius = 10;
+let pi = 3.1415926535;
+
+// d = r + r
+let diameter = radius + radius;
+// C = π(2r) = πd
+let circumference = pi * diameter;
+// A = πr²
+let area = pi * radius * radius;
+
+// reassign variables
+radius = 5;
+diameter = radius * 2;
+console.log(diameter);
+
+let tableLength = 100;
+console.log(tableLength % 69);
+
+let name = "yangming";
+let age = 16;
+let isMale = true;
+
+if (age >= 18) {
+  console.log(name + " can drink 😎");
+}
+else {
+  if (isMale) {
+    console.log(name + " will probably drink anyway 🧔");
+  }
+}
+
+
+let length = 10;
+let width = 5;
+
+_____ area = _____ __ _____;
+
+if (area __ 100) {
+  console.log("wow that's so big 🥵");
+}
+else {
+  console.log("ew it's so small 🤢");
+}
+
+
+function add(a, b) {
+  return a + b;
+}
+function square(x) {
+  return x * x;
+}
+function sayHi() {
+  console.log("hi uwu");
+}
+
+
+let numberOfApples = add(3, 4);
+let areaOfGarden = square(10);
+
+sayHi();
+console.log(
+  "my garden of " + areaOfGarden + " m² can make " + numberOfApples + " apples a day!"
+);
+
+
+function introduction_1(name) {
+  console.log("hi! my name is " + name);
+}
+function introduction_2(name) {
+  return console.log("hi! my name is " + name);
+}
+
+function doSomething() {
+  console.log("meow");
+}
+
+
+//https://gist.github.com/Fogeinator/0d55090d49658cf2179579862c2ee683
+//https://docs.google.com/presentation/d/1MK967s_jE_25BOkJSDaL5VupZvIC0zixUjCvYLCpIOE/edit#slide=id.ge290800b28_0_79
